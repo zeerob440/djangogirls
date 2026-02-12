c2a388c6b3797e0359202369533f6572fca4f641
zeerob440 <zeerob440@tutanota.com>
2026-02-12 15:53:23 -0500

    debug: server now running, blog missing.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    return render(request, 'blog/post_edit.html', {})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_edit.html', {'post': post})
