36def974421f6830f25e2ace5abfcd748e017e73
zeerob440 <zeerob440@tutanota.com>
2026-02-12 16:36:24 -0500

    debug: attempt to display posts

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,8 @@ from .models import Post
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
