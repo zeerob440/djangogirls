435115d0ba78692c1905a06cb5fa411f77e9cae9
zeerob440 <zeerob440@tutanota.com>
2026-02-19 12:37:44 -0500

    feat: dates added to blog posts via terminal

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .models import Post
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now.orderby('published_date'))
+    posts = Post.objects.all().order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
